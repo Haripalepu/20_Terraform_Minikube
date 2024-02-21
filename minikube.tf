1fe4167402e30805492996bc5650f57870c4187b
hari <hari@gmail.com>
2024-02-22 00:40:22 +0530

    adding

---
+++ minikube.tf
@@ -0,0 +1,22 @@
+module "minikube" {
+  source = "github.com/scholzj/terraform-aws-minikube"
+
+  aws_region    = "us-east-1"
+  cluster_name  = "Minikube"
+  aws_instance_type = "t3.medium"
+  ssh_public_key = "~/devopskey.pub" #Public key 
+  aws_subnet_id = "subnet-082a719628d901b26" #replace your default subnet id
+  hosted_zone = "haripalepu.cloud"
+  hosted_zone_private = false
+
+  tags = {
+    Application = "Minikube"
+  }
+
+  addons = [
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/storage-class.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/heapster.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/dashboard.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/external-dns.yaml"
+  ]
+}
